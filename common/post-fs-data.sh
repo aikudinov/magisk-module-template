c3b33312a8c9fb4e26791ae9b0de93874c22fcbe
Andrey Kudinov <andrey@kudinov.su>
2018-02-19 22:22:17 +0300

    Copied ro.build.fingerprint from universal-safetynet-fix. Left some other resetprop stuff in, removed all the remounting and extra files - Xiaomi mi6 currently needs only first resetprop to pass safetynet.

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -1,7 +1,44 @@
 #!/system/bin/sh
-# Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
-# This will make your scripts compatible even if Magisk change its mount point in the future
+
 MODDIR=${0%/*}
 
-# This script will be executed in post-fs-data mode
-# More info in the main Magisk thread
+set -x
+
+RESETPROP="resetprop -v -n"
+
+if [ -f "/sbin/magisk" ]; then RESETPROP="/sbin/magisk $RESETPROP"
+elif [ -f "/data/magisk/magisk" ]; then RESETPROP="/data/magisk/magisk $RESETPROP"
+elif [ -f "/magisk/.core/bin/resetprop" ]; then RESETPROP=/magisk/.core/bin/$RESETPROP
+elif [ -f "/data/magisk/resetprop" ]; then RESETPROP=/data/magisk/$RESETPROP; fi
+
+FINGERPRINT=Xiaomi/sagit/sagit:7.1.1/NMF26X/V8.2.17.0.NCACNEC:user/release-keys
+$RESETPROP "ro.build.fingerprint" "$FINGERPRINT"
+$RESETPROP "ro.bootimage.build.fingerprint" "$FINGERPRINT"; }
+
+# This is not needed on Xiaomi Mi6, but maybe it helps someone. 
+
+VERIFYBOOT=$(getprop ro.boot.verifiedbootstate)
+FLASHLOCKED=$(getprop ro.boot.flash.locked)
+VERITYMODE=$(getprop ro.boot.veritymode)
+KNOX1=$(getprop ro.boot.warranty_bit)
+KNOX2=$(getprop ro.warranty_bit)
+DEBUGGABLE=$(getprop ro.debuggable)
+SECURE=$(getprop ro.secure)
+BUILDTYPE=$(getprop ro.build.type)
+BUILDTAGS=$(getprop ro.build.tags)
+BUILDSELINUX=$(getprop ro.build.selinux)
+RELOADPOLICY=$(getprop selinux.reload_policy)
+
+[ "$VERIFYBOOT" ] && [ "$VERIFYBOOT" != "green" ] && $RESETPROP "ro.boot.verifiedbootstate" "green"
+[ "$FLASHLOCKED" ] && [ "$FLASHLOCKED" != "1" ] && $RESETPROP "ro.boot.flash.locked" "1"
+[ "$VERITYMODE" ] && [ "$VERITYMODE" != "enforcing" ] && $RESETPROP "ro.boot.veritymode" "enforcing"
+[ "$KNOX1" ] && [ "$KNOX1" != "0" ] && $RESETPROP "ro.boot.warranty_bit" "0"
+[ "$KNOX2" ] && [ "$KNOX2" != "0" ] && $RESETPROP "ro.warranty_bit" "0"
+[ "$DEBUGGABLE" ] && [ "$DEBUGGABLE" != "0" ] && $RESETPROP "ro.debuggable" "0"
+[ "$SECURE" ] && [ "$SECURE" != "1" ] && $RESETPROP "ro.secure" "1"
+[ "$BUILDTYPE" ] && [ "$BUILDTYPE" != "user" ] && $RESETPROP "ro.build.type" "user"
+[ "$BUILDTAGS" ] && [ "$BUILDTAGS" != "release-keys" ] && $RESETPROP "ro.build.tags" "release-keys"
+[ "$BUILDSELINUX" ] && [ "$BUILDSELINUX" != "0" ] && $RESETPROP "ro.build.selinux" "0"
+[ "$RELOADPOLICY" ] && [ "$RELOADPOLICY" != "1" ] && $RESETPROP "selinux.reload_policy" "1"
+
+exit
--- config.sh
+++ config.sh
@@ -22,13 +22,13 @@
 
 # Set to true if you need to enable Magic Mount
 # Most mods would like it to be enabled
-AUTOMOUNT=true
+AUTOMOUNT=false
 
 # Set to true if you need to load system.prop
 PROPFILE=false
 
 # Set to true if you need post-fs-data script
-POSTFSDATA=false
+POSTFSDATA=true
 
 # Set to true if you need late_start service script
 LATESTARTSERVICE=false
@@ -41,7 +41,7 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
+  ui_print "  Safetynet fingerprint fix    "
   ui_print "*******************************"
 }
 
